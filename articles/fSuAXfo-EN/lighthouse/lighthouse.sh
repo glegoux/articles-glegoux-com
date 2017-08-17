a001b90c4ec06a6319e4c5697cbfe53e4c6be6fd
Gilles Legoux <gilles.legoux@gmail.com>
2017-08-17 23:56:13 +0200

    Update lighthouse.sh

--- articles/fSuAXfo-EN/lighthouse/lighthouse.sh
+++ articles/fSuAXfo-EN/lighthouse/lighthouse.sh
@@ -2,15 +2,15 @@
 #
 # lighthouse.sh
 #
-# Generate LightHouse json rapports
+# Generate LightHouse json reports
 #
 # Execute: ./lighthouse.sh
 
 i=0
 for url in $(cat urls.txt); do
   echo -n "--"
-  echo "$i ${url}" | tee -a matching_url_rapport.txt
+  echo "$i ${url}" | tee -a matching_url_report.txt
   echo -n "--"
-  lighthouse "${url}" --output=json --output-path="rappot-$i.json"
+  lighthouse "${url}" --output=json --output-path="report-$i.json"
   $((i++))
 done
